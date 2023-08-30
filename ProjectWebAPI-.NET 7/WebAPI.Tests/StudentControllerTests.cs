0e93e93afed20b67bcd1446a04509d427648a82b
kovac031 <ivan.kovac.email@gmail.com>
2023-08-30 14:59:08 +0200

    did something

--- ProjectWebAPI-.NET 7/WebAPI.Tests/StudentControllerTests.cs
+++ ProjectWebAPI-.NET 7/WebAPI.Tests/StudentControllerTests.cs
@@ -5,6 +5,7 @@ using Service.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using WebAPI.Controllers;
@@ -20,8 +21,10 @@ namespace WebAPI.Tests
             _service = A.Fake<IService>(); //FakeItEasy
             _controller = new StudentController(_service); // SUT, system under test
         }
+
+        //----------------- GET ALL / LIST -------------- // moj GetAllAsync ima dva returna, listu ako uspije i exception ako ne, testam to dvoje
         [Fact]
-        public async Task StudentController_GetAllAsync_ReturnList()
+        public async Task StudentController_GetAllAsync_ReturnList() // lista
         {
             // Arrange
             A.CallTo(() => _service.GetAllAsync()).Returns(new List<StudentDTO>());
@@ -31,26 +34,70 @@ namespace WebAPI.Tests
 
             // Assert
             var okResult = Assert.IsType<OkObjectResult>(result);
-
-            // Ensure that the service method was called
-            A.CallTo(() => _service.GetAllAsync()).MustHaveHappenedOnceExactly();
+            Assert.IsType<List<StudentDTO>>(okResult.Value);
         }
+        [Fact]
+        public async Task StudentController_GetAllAsync_ThrowException() // exception
+        {
+            // Arrange
+            var exception = "Database connection error";
+            A.CallTo(() => _service.GetAllAsync()).Throws(new Exception(exception));
+
+            // Act
+            var result = await _controller.GetAllAsync();
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, objectResult.StatusCode);
+            Assert.Equal($"Error for GetAllAsync: {exception}", objectResult.Value);
+        }
+        // ---------------- GET ONE BY ID ---------------
+        [Fact]
         public async Task StudentController_GetOneByIdAsync_ReturnObject()
         {
+            // Arrange
+            Guid id = Guid.NewGuid(); // moze i konkretan guid tu
+            A.CallTo(() => _service.GetOneByIdAsync(id)).Returns(new StudentDTO());
 
+            // Act
+            var result = await _controller.GetOneByIdAsync(id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.IsType<StudentDTO>(okResult.Value);
         }
+        [Fact]
+        public async Task StudentController_GetOneByIdAsync_ThrowException() 
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+            var exception = "Database connection error";
+            A.CallTo(() => _service.GetOneByIdAsync(id)).Throws(new Exception(exception));
+
+            // Act
+            var result = await _controller.GetOneByIdAsync(id);
+
+            // Assert
+            var objectResult = Assert.IsType<ObjectResult>(result);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, objectResult.StatusCode);
+            Assert.Equal($"Error for GetOneByIdAsync: {exception}", objectResult.Value);
+        }
+        // ------------------- CREATE ---------------
         public async Task StudentController_CreateAsync_ReturnBool()
         {
             // one automatske se moze provjeriti npr registered on, je li prije ili poslije nekog datuma
         }
+        // -------------------- EDIT ---------------
         public async Task StudentController_EditAsync_ReturnBool()
         {
 
         }
+        // ------------------- DELETE ---------------
         public async Task StudentController_DeleteAsync_ReturnBool()
         {
 
         }
+        // ------------ GET LIST WITH PARAMETERS ---------------
         public async Task StudentController_ParamsAsync_ReturnList()
         {
 
