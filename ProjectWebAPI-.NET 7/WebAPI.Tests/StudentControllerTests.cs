7d3ec9609a50e1b325a4a5f4db0af422b12d57ee
kovac031 <ivan.kovac.email@gmail.com>
2023-08-30 12:57:35 +0200

    Added xUnit project for .NET7 WebAPI project, installed and set up FakeItEasy for mocking, started on tests

---
+++ ProjectWebAPI-.NET 7/WebAPI.Tests/StudentControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests
+{
+    public class StudentControllerTests
+    {
+        private readonly StudentController _controller;
+        private readonly IService _service;
+        public StudentControllerTests()
+        {
+            _service = A.Fake<IService>(); //FakeItEasy
+            _controller = new StudentController(_service); // SUT, system under test
+        }
+        [Fact]
+        public async Task StudentController_GetAllAsync_ReturnList()
+        {
+            // Arrange
+            A.CallTo(() => _service.GetAllAsync()).Returns(new List<StudentDTO>());
+
+            // Act
+            var result = await _controller.GetAllAsync();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+
+            // Ensure that the service method was called
+            A.CallTo(() => _service.GetAllAsync()).MustHaveHappenedOnceExactly();
+        }
+        public async Task StudentController_GetOneByIdAsync_ReturnObject()
+        {
+
+        }
+        public async Task StudentController_CreateAsync_ReturnBool()
+        {
+            // one automatske se moze provjeriti npr registered on, je li prije ili poslije nekog datuma
+        }
+        public async Task StudentController_EditAsync_ReturnBool()
+        {
+
+        }
+        public async Task StudentController_DeleteAsync_ReturnBool()
+        {
+
+        }
+        public async Task StudentController_ParamsAsync_ReturnList()
+        {
+
+        }
+    }
+}
---
+++ ProjectWebAPI-.NET 7/WebAPI.Tests/StudentServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Tests
+{
+    public class StudentServiceTests
+    {
+        public void StudentService_GetAllAsync_ReturnList()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+        public void StudentService_GetOneByIdAsync_ReturnObject()
+        {
+
+        }
+        public void StudentService_CreateAsync_ReturnBool()
+        {
+
+        }
+        public void StudentService_EditAsync_ReturnBool()
+        {
+
+        }
+        public void StudentService_DeleteAsync_ReturnBool()
+        {
+
+        }
+        public void StudentService_ParamsAsync_ReturnList()
+        {
+
+        }
+    }
+}
