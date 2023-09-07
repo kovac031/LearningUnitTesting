2f6ebcd45fbed77967ad65d0cb56f4c2f62beb29
kovac031 <ivan.kovac.email@gmail.com>
2023-09-07 10:42:54 +0200

    getallasync test for service layer done

--- ProjectWebApi-.NET 4.7.2/WebAPI.Tests/ServiceTests/StudentServiceTests.cs
+++ ProjectWebApi-.NET 4.7.2/WebAPI.Tests/ServiceTests/StudentServiceTests.cs
@@ -8,6 +8,9 @@ using System.Web.Http.Hosting;
 using System.Web.Http;
 using Repository.Common;
 using Service;
+using Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebAPI.Tests.ServiceTests
 {
@@ -25,6 +28,32 @@ namespace WebAPI.Tests.ServiceTests
         }
         //-------------------------------------------------------------------
 
+        [TestMethod]
+        public async Task GetAllAsync_OnSuccess_ReturnList()
+        {
+            // Arrange
+            List<StudentDTO> fakeList = HelperClass.GetFakeStudents();
+            _repository.Setup(x => x.GetAllAsync()).ReturnsAsync(fakeList);
+
+            // Act
+            List<StudentDTO> result = await _service.GetAllAsync();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(fakeList.Count, result.Count);
+        }
+
+        [TestMethod]
+        public async Task GetAllAsync_OnFail_ThrowsException()
+        {
+            // Arrange
+            _repository.Setup(x => x.GetAllAsync()).ThrowsAsync(new Exception("MY TEST EXCEPTION"));
+
+            // Act & Assert
+            await Assert.ThrowsExceptionAsync<Exception>(() => _service.GetAllAsync());
+        }
+
+        //-----------------------------------------------------------------
 
     }
 }
